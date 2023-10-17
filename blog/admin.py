f9492d921b9eb3879ed219901caa0b6cdf2e05d4
alinevita <aline.vita@alternativasistemas.com.br>
2023-10-17 10:20:31 -0300

    atualizações

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Tag, Author, Post
 
 # Register your models here.
+
+class PostsAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostsAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
